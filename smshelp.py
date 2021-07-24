43108ac9de5384c4e4dd1ad8cf0b324f4fd64428
Jeon Min Seok <76759687+minseokhouse@users.noreply.github.com>
2021-07-24 11:55:02 +0900

    Update smshelp.py

--- smshelp.py
+++ smshelp.py
@@ -6,19 +6,19 @@ import requests
 import json
 
 def make_signature(string):
-    secret_key = bytes("iiP7U0sqph3glBH1rCJXizgNlut1O5Msy46EOhhC", 'UTF-8')
+    secret_key = bytes("iiP7U0sq중요키*** 일부 숨김", 'UTF-8')
     string = bytes(string, 'UTF-8')
     string_hmac = hmac.new(secret_key, string, digestmod=hashlib.sha256).digest()
     string_base64 = base64.b64encode(string_hmac).decode('UTF-8')
     return string_base64
 
-message = "Hello This is python test   hangle i an dae yo. I am Minseok"
-phone = "01038624194"
+message = "보내는 메시지"
+phone = "수신번호"
 url = "https://sens.apigw.ntruss.com"
-uri = "/sms/v2/services/" + "ncp:sms:kr:270154268312:gpshelp" + "/messages"
+uri = "/sms/v2/services/" + "ncp:sms:kr:2701중요키*** 일부 숨김:gpshelp" + "/messages"
 api_url = url + uri
 timestamp = str(int(time.time() * 1000))
-access_key = "xJ8jC4LIUsc5XZH3KQ5T"
+access_key = "xJ8jC4L중요키*** 일부 숨김"
 string_to_sign = "POST " + uri + "\n" + timestamp + "\n" + access_key
 signature = make_signature(string_to_sign)
 
@@ -33,7 +33,7 @@ headers = {
 body = {
         "type": "SMS",
         "contentType": "COMM",
-        "from": "01038624194",
+        "from": "등록된 번호",
         "content": message,
         "messages": [{"to": phone}]
 }
